b6e967e6910cf8623f1f229ad7b91e2de13617c6
CoolyFooly <franek1519@gmail.com>
2024-08-24 23:30:42 +0200

    Empty executor class

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
+#include "endpoint.hpp"
+
 #include <string>
 #include <iostream>
+#include <cstdlib>
 #include <execution>
 #include <thread>
+#include <cstring>
 #include <functional>
 #include <sys/signal.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
-
+#include <arpa/inet.h>
 
 void callback() {
 
@@ -21,36 +25,28 @@ namespace asyncio {
     };
 
     namespace tcp {
-        
-        class endpoint {
-
-        public:
-            endpoint(std::string address, short port) {
-                server_addr.sin_family = AF_INET;
-                server_addr.sin_addr.s_addr = htonl(INADDR_ANY); //default address
-                server_addr.sin_port = htons(port);
-            }
-
-            endpoint(const char *address, short port);
-
-
-        private:
-            sockaddr_in server_addr;
-
-            std::string address;
-            short int port;
-        };
 
         class socket {
             typedef std::function<void(asyncio::error, int)> AsyncCallback;
+        public:
 
-            void async_connect(tcp::endpoint endpoint) {
+            ~socket() {
+                close(file_descriptor);
+            }
+
+            void async_connect(tcp::endpoint endpoint, AsyncCallback callback) {
                 file_descriptor = ::socket(AF_INET, SOCK_STREAM, 0);
-
+                
                 if(file_descriptor == -1) {
                     //throw error
+                    std::cout << "ERROR CREATING SOCKET\n";
+                }
+                if(bind(file_descriptor, (const sockaddr*)&endpoint.server_addr, sizeof(endpoint.server_addr)) < 0) {
+                    //throw bind eerror
+                    std::cout << "BIND ERROR\n";
                 }
 
+                std::cout << "LOL\n";
 
 
             }
@@ -63,6 +59,7 @@ namespace asyncio {
 
             }
 
+        private:
             int file_descriptor = 0;    
         };
 
@@ -72,6 +69,13 @@ namespace asyncio {
 
 int main() {
 
+    asyncio::tcp::endpoint ep(3001);
+    asyncio::tcp::socket socket;
+    socket.async_connect(ep, [](asyncio::error error, int size){
+
+    });
+
+    while(1){}
 
     return 0;
 }
