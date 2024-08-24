e8dbd5c89d1f5d1514139b83c6b30643e9fef7a0
CoolyFooly <franek1519@gmail.com>
2024-08-24 23:31:42 +0200

    Executor class

---
+++ executor.hpp
@@ -0,0 +1,19 @@
+#ifndef EXECUTOR_HPP
+#define EXECUTOR_HPP
+
+
+namespace asyncio {
+
+class executor {
+
+
+
+    void event_loop() {
+        while(1) {
+            
+        }    
+    }
+
+};
+}
+#endif
