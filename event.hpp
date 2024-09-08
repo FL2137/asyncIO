ce1efd29237ed6de00886bb964518a0ad144e048
CoolyFooly <franek1519@gmail.com>
2024-09-08 22:09:56 +0200

    event now holds its completion token shr_ptr

--- event.hpp
+++ event.hpp
@@ -17,9 +17,29 @@ enum EVENT_TYPE {
     SOCKET_IO,
 };
 
+
+template <typename T>
+class promise {
+public:
+    promise() {
+
+    }
+
+    T result;
+};
+
+class Token {
+    public:
+    Token(){}
+    bool state;
+    std::string event_name;
+};
+
+
 class event {
 public: 
     typedef std::function<void(asyncio::error, int)> AsyncCallback;
+    typedef std::function<void(void)> Callback;
 
     event(std::string event_name, AsyncCallback event_handler, EVENT_TYPE event_type = EVENT_TYPE::SOCKET_IO) {
         this->name = event_name;
@@ -33,21 +53,57 @@ public:
         }
     }
 
+    event(std::string event_name, Callback empty_callback, EVENT_TYPE event_type = EVENT_TYPE::SYSTEM_IO) {
+        this->name = event_name;
+        this->call = empty_callback;
+        this->type = event_type;
+    }
+
+    void set_data(asyncio::error error, int nbytes) {
+        is_child = true;
+        child_error = error;
+        child_nbytes = nbytes;
+    }
+
+    void run() {
+        status = WORKING;
+        if(type == SYSTEM_IO) {
+            call();
+        }
+        else if(type == SOCKET_IO) {
+            handler(child_error, child_nbytes);
+        }
+    }
+
     operator bool() const{
         if(status == PENDING)
             return true;
         return false;
     }
+    
+    void set_token(std::shared_ptr<Token> new_token) {
+        this->token = token;
+    }
+
+    std::shared_ptr<Token> token;
+
 public:
 
     int priority;
+    EVENT_STATUS status = PENDING;
+    std::string name;
 
 
 private:
+    bool is_child = false;
+    asyncio::error child_error;
+    int child_nbytes = -1;
+
     AsyncCallback handler;
-    EVENT_STATUS status;
+    
+
+    Callback call;
     EVENT_TYPE type;
-    std::string name;
 };
 
 }
