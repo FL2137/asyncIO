d4097ac9810b215d33e801dcfebe5d4d1f6bdd0e
CoolyFooly <franek1519@gmail.com>
2024-09-10 22:17:58 +0200

    some epoll stuff

--- executor.hpp
+++ executor.hpp
@@ -7,7 +7,15 @@
 #include <iostream>
 #include "event.hpp"
 #include <thread>
+#include <signal.h>
+#include <unistd.h>
+#include <any>
+#include <map>
+#include <sys/epoll.h>
+#include <mutex>
+#include <shared_mutex>
 
+#define EPOLL_COUNT 50
 
 namespace asyncio {
 
@@ -16,16 +24,29 @@ class executor {
     typedef std::function<void(void)> Callback;
 
     void event_loop() {
-        while(1) {
+        signal(SIGINT, [](int){
+            std::cout << "LOL\n";
+            executor::runner = false;});
+
+        while(runner) {
             process_events();
-            sleep(1);
         }    
     }
 
-
 public:
+
+    executor() {
+        epoll_fd = epoll_create(EPOLL_COUNT);
+    }
+
+    inline static bool runner = true;
+
     void run() {
         event_loop();
+    }
+
+    void catch_signal(int dummy) {
+
     }
 
     static void register_event(executor &exec, event event) {
@@ -37,48 +58,62 @@ public:
     }
 
     void run_thread(event event) {
-        std::thread worker_thread(&event::run, event);
+        auto token = std::make_shared<Token>();
+        token->event_name = event.name;
+        event.set_token(token);
+        //tokens[reinterpret_cast<int32_t>(&event)] = token;
 
+        std::thread worker_thread(&event::run, event);
         worker_thread.detach();
     }
 
-    void run_thread(Callback foo) {
-        auto lamb = []() {
-            std::cout << "peepee\n";
-        };
-
-        std::thread tr(lamb);   
-        tr.detach();
-        sleep(1);
-        if(!tr.joinable()) {
-            std::cout << "its detached u moron\n";
-        }
-    }
-
 private:
-    std::deque<event> events;
-    
+    int epoll_fd = 0;
+    epoll_event epoll_events[EPOLL_COUNT];
+    std::vector<event> events;
+    std::map<event, std::shared_ptr<Token>> tokens;
+
     void process_events() {
-        std::sort(events.begin(), events.end(), [](const event& lhs, const event& rhs) {
-            if(lhs.priority >= rhs.priority)
-                return true;
-            else return false;
-        });
+        // std::sort(events.begin(), events.end(), [](const event& lhs, const event& rhs) {
+        //     if(lhs.priority >= rhs.priority)
+        //         return true;
+        //     else return false;
+        // });
 
         // while(events.size()) {
         //     std::cout << "Running new event...\n";
         //     run_thread(events[0]);
         //     //events.pop_front();
         // }
-        for(auto event : events) {
+
+
+
+
+        for(auto &event : events) {
             if(event.status == PENDING) {
+                event.status = WORKING;
+                std::cout << event.name <<std::endl;
                 run_thread(event);
             }
+            else {
+                std::cout << event.name << " " << events.size() << std::endl;
+            }
         } 
-
-
     }
 
+
+    mutable std::shared_mutex token_mutex;
+
+    void process_tokens() {
+        std::unique_lock lock(token_mutex);
+        for(auto &token : tokens) {
+            if(token.second->completed) {
+                auto iter = std::find(events.begin(), events.end(), token.first);
+                iter->status = COMPLETED;
+
+            }
+        }
+    }
 };
 }
 #endif
