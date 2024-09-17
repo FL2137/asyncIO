a49e36f0c7890ae773cd133b2f6e43364ee2f052
CoolyFooly <franek1519@gmail.com>
2024-09-17 23:48:43 +0200

    WriteToken class skeleton

--- callback.hpp
+++ callback.hpp
@@ -3,43 +3,75 @@
 #include <any>
 #include <functional>
 #include <memory>
+#include <iostream>
 
 
 
 namespace asyncio {
+typedef std::function<void(asyncio::error, int)> IO_Signature; 
 
 //template <typename ...Args>
 class Callback {
 public:
-    Callback();
+    Callback() {
+
+    }
     
     virtual void call() {
-    }
-
-    std::function<void(void)> functor;
-};
-
-template<typename ...Args>
-class ReadToken : Callback {
-public:
-
-    ReadToken() {
-
-    }
-
-    void call() override {
         functor();
     }
 
-    
+    std::function<void(void)> functor = []() {
+    };
 
-    std::function<void(Args ...args)> functor;
+
+private:
 };
 
-void runXd() {  
-    std::vector<Callback> vec = {};
-    ReadToken<int, char> rt;
+class ReadToken : public Callback {
+public:
 
+    ReadToken(IO_Signature completion_handler) {
+        this->functor = completion_handler;
+    }
+
+    ReadToken(IO_Signature completion_handler, asyncio::error error, int nbytes) {
+        this->functor = completion_handler;
+        m_error = error;
+        m_nbytes = nbytes;
+    }
+
+    void set_data(asyncio::error _err, int _nbytes) {
+        m_error = _err;
+        m_nbytes = _nbytes;
+    }
+
+    void call() override {
+        functor(m_error, m_nbytes);
+    }
+
+private:
+    asyncio::error m_error;
+    int m_nbytes;
+    std::function<void(asyncio::error, int)> functor;
+};
+
+
+class WriteToken : public Callback {
+    WriteToken(IO_Signature completion_handler) {
+
+    }
+
+    IO_Signature functor;
+};
+    
+void runXd() {  
+    std::vector<Callback*> vec = {};
+    ReadToken rt(
+    [](asyncio::error error, int nbytes)
+        {
+            std::cout << "Derived class FNCTOR\n";
+        }, asyncio::error(), 15);
 }
 
 
