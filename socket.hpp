9c1df06cb6157fb4833894a814ca2d3834e968a3
CoolyFooly <franek1519@gmail.com>
2024-10-17 23:09:03 +0200

    socket async operations implementations

--- socket.hpp
+++ socket.hpp
@@ -22,12 +22,60 @@ namespace asyncio {
             }
 
             void async_read_some(char *buffer, int size, ReadCallback callback) {
+                this->read_callback = callback;
+                this->read_buffer = buffer;
+                this->read_size = size;
+
+                epoll_read_event.events = EPOLLIN | EPOLLET;
+                epoll_read_event.data.fd = fd;
+                epoll_read_event.data.ptr = read_buffer;
+                epoll_read_event.data.u32 = executor::callback_id;
+
+                Token *impl = new Token();
+
+                impl->callback = std::bind(&socket::read_impl, this);
+                impl->name = "ReadImpl." + std::to_string(fd);
+
+                executor.register_epoll_handler(impl);
+                executor.register_epoll(&epoll_read_event);
+
             }
 
             void async_write_some(char *buffer, int size, WriteCallback callback) {
+                //rearm oneshot EPOLLOUT event
+                if(write_callback) {
+                    
+                    epoll_write_event.events = EPOLLONESHOT | EPOLLOUT | EPOLLET;
+                    epoll_write_event.data.fd = fd;
+                    epoll_write_event.data.ptr = write_buffer;
+
+                    executor.epoll_rearm(fd, &epoll_write_event);
+                    return;
+                }
+
+                this->write_callback = callback;
+                this->write_buffer = buffer;
+                this->write_size = size;
+
+                epoll_write_event.events = EPOLLONESHOT | EPOLLOUT | EPOLLET;
+                epoll_write_event.data.fd = fd;
+                epoll_write_event.data.ptr = write_buffer;
+                epoll_write_event.data.u32 = executor::callback_id;
+
+                executor.register_epoll(&epoll_write_event);
+
+                Token *impl = new Token();
+                impl->callback = std::bind(&socket::write_impl, this);
+                impl->name = "WriteImpl." + std::to_string(fd);
+                
+                executor.register_epoll_handler(impl);
+
+
+                
             }   
 
             void read_impl() {
+                std::cout << "READ IMPLE CALLED\n";
                 asyncio::error error;
                 int result = read(fd,  read_buffer, read_size);
                 if(result == -1) {
