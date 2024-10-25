175892e1d3bc2fec93e8e559b3a6fd980ff1ab36
CoolyFooly <franek1519@gmail.com>
2024-10-25 23:27:06 +0200

    impl callback for async_write() takes what it should by value

--- asyncio.hpp
+++ asyncio.hpp
@@ -9,16 +9,29 @@
 namespace asyncio {
 
     void async_write(const asyncio::tcp::socket &socket, char *buffer, int size, WriteCallback callback) {
-                
+
+
+        // int r = write(socket.fd, buffer, size);
+        // if(r == -1) {
+        //     std::cout << "error writing\n";
+        //     return;
+        // }
+
+        // std::cout << "lol ok\n";
+
         Token *impl_token = new Token();
+        int sfd = socket.fd;
+
 
         impl_token->name = "write_impl_token";
-        impl_token->callback = [&]() {
-            asyncio::error error;
-            int result = write(socket.fd, buffer, size);
 
+        impl_token->callback = [&, sfd, buffer, size, callback]() {
+
+            asyncio::error error;
+
+            int result = write(sfd, buffer, size);
             if(result == -1) {
-                error.set_error_message("Read error");
+                error.set_error_message("Write error: " + std::to_string(errno));
             }
 
             WriteToken *write_token = new WriteToken(callback);
