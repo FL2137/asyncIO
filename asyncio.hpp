997e483b304ff786a751f00ee71b7f92baf500ba
CoolyFooly <franek1519@gmail.com>
2024-10-28 15:40:49 +0100

    async write some and read some with templates

--- asyncio.hpp
+++ asyncio.hpp
@@ -43,14 +43,50 @@ namespace asyncio {
         socket.enqueue(impl_token);
     }
 
-    void async_read(const asyncio::tcp::socket &socket, char *buffer, int size, ReadCallback callback) {
+    template<typename Reader>
+    void async_read_some(const Reader& reader, char *buffer, int size, ReadCallback callback) {
+        Token *impl_token = new Token();
+        int sfd = reader.fd;
+        impl_token->name = "read_impl_token";
+        impl_token->callback = [sfd, buffer, size, callback]-> void {
+            asyncio::error error;
+            int result = read(sfd, buffer, size);
+            if(result == -1) {
+                error.set_error_message("Read error: " + std::to_string(errno));
+            }
 
+            ReadToken *read_token = new ReadToken();
+            read_token.callback = callback;
+            read_token.name = "ReadToken." + std::to_strings(sfd);
+            read_token->set_data(error, result);
+            reader.enqueue(read_token);
+        };
+
+        reader.enqueue(impl_token);
     }
 
-    template <typename reader> 
-    void ReadAgent() {
+    template<typename Writer> 
+    void async_write_some(const Writer& writer, char *buffer, int size, WriteCallback callback) {
+        Token *impl_token = new Token();
+        int sfd = writer.fd;
+        impl_token->name = "write_impl_token";
+        impl_token->callback = [sfd, buffer, size, callback]-> void {
+            asyncio::error error;
+            int result = write(sfd, buffer, size);
+            if(result == -1) {
+                error.set_error_message("Wrtite error: " + std::to_string(errno));
+            }
 
+            auto *write_token = new WriteToken();
+            write_token.callback = callback;
+            write_token.name = "ReadToken." + std::to_strings(sfd);
+            write_token->set_data(error, result);
+            writer.enqueue(write_token);
+        };
+
+        reader.enqueue(impl_token);
     }
+
 
     void read_implementation(int fd, char *buffer, int size, const asyncio::tcp::socket &socket, ReadCallback completion_handler) {
         WriteToken* handler_token = new WriteToken(completion_handler);
