07592377808e86efac2bd4084d6afa87728ab3c4
CoolyFooly <franek1519@gmail.com>
2024-10-29 15:34:20 +0100

    async_accept_one function

--- asyncio.hpp
+++ asyncio.hpp
@@ -43,21 +43,50 @@ namespace asyncio {
         socket.enqueue(impl_token);
     }
 
+    template<typename Acceptor, typename Socket> 
+    void async_accept_one(const Acceptor &acceptor, Socket &socket, AcceptCallback callback) {
+        Token *impl_token = new Token();
+        impl_token->name = "accept_one_impl_token";
+        impl_token->callback = [&, callback] {
+            asyncio::error error;
+            
+            socklen_t socklen;
+            sockaddr_in remote_endpoint;
+            
+            int result_fd = accept(acceptor.fd, (sockaddr*)&remote_endpoint, &socklen);
+
+            if(result_fd == -1) {
+                error.set_error_message("accept error: " + std::to_string(errno));
+            }
+            if(fcntl(result_fd, F_SETFL, fcntl(result_fd, F_GETFL) | O_NONBLOCK) == -1) {
+                std::cout << "ERROR SETTING NONBLOCK ON SOCKET FD";
+            }
+            socket.setup(result_fd);
+
+            AcceptToken *accept_token = new AcceptToken(callback);
+            accept_token->name = "accept_one_token";
+            accept_token->set_data(error);
+
+            acceptor.enqueue(accept_token);
+        };
+
+        acceptor.enqueue(impl_token);
+    }
+
     template<typename Reader>
     void async_read_some(const Reader& reader, char *buffer, int size, ReadCallback callback) {
         Token *impl_token = new Token();
         int sfd = reader.fd;
         impl_token->name = "read_impl_token";
-        impl_token->callback = [sfd, buffer, size, callback]-> void {
+        impl_token->callback = [&, sfd, buffer, size, callback] {
             asyncio::error error;
             int result = read(sfd, buffer, size);
             if(result == -1) {
                 error.set_error_message("Read error: " + std::to_string(errno));
             }
 
-            ReadToken *read_token = new ReadToken();
-            read_token.callback = callback;
-            read_token.name = "ReadToken." + std::to_strings(sfd);
+            ReadToken *read_token = new ReadToken(callback);
+            read_token->name = "ReadToken." + std::to_string(sfd);
             read_token->set_data(error, result);
             reader.enqueue(read_token);
         };
@@ -70,21 +99,20 @@ namespace asyncio {
         Token *impl_token = new Token();
         int sfd = writer.fd;
         impl_token->name = "write_impl_token";
-        impl_token->callback = [sfd, buffer, size, callback]-> void {
+        impl_token->callback = [&, sfd, buffer, size, callback] {
             asyncio::error error;
             int result = write(sfd, buffer, size);
             if(result == -1) {
                 error.set_error_message("Wrtite error: " + std::to_string(errno));
             }
 
-            auto *write_token = new WriteToken();
-            write_token.callback = callback;
-            write_token.name = "ReadToken." + std::to_strings(sfd);
+            auto *write_token = new WriteToken(callback);
+            write_token->name = "WriteToken." + std::to_string(sfd);
             write_token->set_data(error, result);
             writer.enqueue(write_token);
         };
 
-        reader.enqueue(impl_token);
+        writer.enqueue(impl_token);
     }
 
 
