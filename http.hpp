e8f49a1a3f78eec7197a702381a3f06cee96b6b7
CoolyFooly <franek1519@gmail.com>
2024-11-08 20:45:49 +0100

    http namespace file

---
+++ http.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+
+
+namespace asyncio {
+namespace HTTP {
+
+
+enum METHOD {
+    GET = 0,
+    POST,
+    DELETE,
+
+};
+
+class request {
+
+};
+
+class response {
+
+};
+
+class parser {
+
+    bool parse(const std::string &message, std::string &result) {
+
+    }
+
+    void f() {
+
+    }
+};
+
+
+} 
+}
